57b40b2d518c371821daaaa18f489f78c622c2da
League Student <league@iMac-22.attlocal.net>
2020-02-13 18:23:42 -0800

    Finish Mad Libs

---
+++ src/section3/Greater.java
@@ -0,0 +1,17 @@
+
+package section3;
+
+import java.awt.JobAttributes;
+
+import javax.swing.JOptionPane;
+
+public class Greater {
+public static void main(String[] args) {
+System.out.println("Sebastian");
+String age =JOptionPane.showInputDialog("how old are you?");
+JOptionPane.showMessageDialog(null,"I am "+ age +" years old");
+
+
+
+}
+}
